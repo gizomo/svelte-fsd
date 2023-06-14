ae16e5b8f78c08cc16a0cb9d3cc16a03f4a89d4c
gizomo <gizomo@ya.ru>
2023-06-14 21:25:24 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  const title: string = 'Svelte FSD';
+</script>
+
+<main>
+  <h1>{title}</h1>
+</main>
+
+<style>
+  main {
+    height: 100%;
+    width: 100%;
+  }
+
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+    padding: 0 40px;
+  }
+</style>
